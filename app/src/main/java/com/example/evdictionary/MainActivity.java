bc05dbab0c2bad37556555519192a895731d7be6
cuongpv2209 <cuongpv2209@gmail.com>
2021-04-03 09:52:18 +0700

    Giao diện cơ bản

---
+++ app/src/main/java/com/example/evdictionary/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.evdictionary;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNav = findViewById(R.id.nav);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, new Homefragment()).commit();
+    }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectFragment = null;
+                    switch (item.getItemId()){
+                        case R.id.item1:
+                            selectFragment = new Homefragment();
+                            break;
+                        case R.id.item2:
+                            selectFragment = new HistoryFragment();
+                            break;
+                        case R.id.item3:
+                            selectFragment = new FavoriteFragment();
+                            break;
+                        case R.id.item4:
+                            selectFragment = new OnlineFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, selectFragment).commit();
+                    return true;
+                }
+            };
+}
